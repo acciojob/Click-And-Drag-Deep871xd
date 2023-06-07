fa65ca28a8ad05097c3be633cfb8e3ac04a84c7a
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-07 19:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Your code here.
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  console.log('walk:', walk); // add this line
+  slider.scrollLeft = scrollLeft - walk;
+  console.log('scrollLeft:', slider.scrollLeft); // add this line
+});
